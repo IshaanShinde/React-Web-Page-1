a5238f278f000ef33a80c8632dcd3e755866e250
Ishaan Shinde <ishaanshinde08@gmail.com>
2023-09-04 07:24:01 +0530

    navigation bar added
    created a navigation component + css for the same

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,25 @@
+const Navigation = () => {
+    return (
+        /* navigation bar */
+        <nav>
+            {/* logo */}
+            <div className="logo">
+                <img src="public/images/logo-flux-echo.jpg" alt="image_logo"/>
+            </div>
+            
+            {/* navigation links */}
+            <ul>
+                <li href="#">Menu</li>
+                <li href="#">About</li>
+                <li href="#">Location</li>
+                <li href="#">Contact</li>
+            </ul>
+
+            {/* login button */}
+            <button>Login</button>
+
+        </nav>
+    )
+}
+
+export default Navigation
